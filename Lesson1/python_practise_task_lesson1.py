3cffc199c27c7279ad122237e0afdad383fb576e
ManuailSol <104797691+ManuailSol@users.noreply.github.com>
2022-08-18 12:41:58 +1200

    Add files via upload

---
+++ Lesson1/python_practise_task_lesson1.py
@@ -0,0 +1,9 @@
+# Manuail Soalqa
+# Practise Task
+
+# Variable declaration
+name = "Manuail"
+height = 1.79
+address = "49 Epuni Street,Aro Vally Wellington,New Zealand"
+phone = "02041116501"
+hasAllergy = False
