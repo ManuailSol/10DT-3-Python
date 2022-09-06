4b6dcb3e6376fcbbffac5a963e86801b05b8d334
ManuailSol <104797691+ManuailSol@users.noreply.github.com>
2022-09-07 11:04:53 +1200

    Add files via upload

---
+++ Lesson2/Problem 2.py
@@ -0,0 +1,10 @@
+# Problem 02
+# This Program is desinged to track the value remaing on a gift card as it is used
+
+Card_Balance = 50.00
+Purchase_01 = 35.00
+print("Your Current Balance: $" + str (Card_Balance))
+
+Card_Balance = Card_Balance - Purchase_01
+print("Purchase_01: $" + str (Purchase_01))
+print("Your New Balance is:  $" + str (Card_Balance))
